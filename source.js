01fef54b2b86de62bf11f65f3dd2990600a88996
kaankarabiber <kaankarabiber1@gmail.com>
2023-09-05 16:04:57 +0300

    Create a temporary solution for negative numbers

--- source.js
+++ source.js
@@ -24,28 +24,54 @@ function operate(a, b, operator){
         return divide(a, b);
     }
 }
+function getDisplay(){
+    return document.querySelector('.display');
+}
 const buttons = document.querySelectorAll('.numbers, .operators');
 buttons.forEach(button => {
     button.addEventListener('click', () =>{
-        const display = document.querySelector('.display');
+        display = getDisplay()
         display.textContent += button.textContent;
     }); 
 });
+const operatorButtons = document.querySelectorAll('.operators');
+operatorButtons.forEach(button =>{
+    button.addEventListener('click', () =>{
+        display = getDisplay();
+        if(display.textContent[0] === '-'){
+            negativeNumber = true;
+        }
+    });
+});
 const clearButton = document.querySelector('.clear');
 clearButton.addEventListener('click', ()=>{
-    const display = document.querySelector('.display');
+    display = getDisplay()
     display.textContent = '';
+    firstNumber = 0;
+    secondNumber = 0;
+    operator = '';
+    negativeNumber = false;
 });
 const equalSignButton = document.querySelector('#equal-sign');
 equalSignButton.addEventListener('click', () =>{
-    const display = document.querySelector('.display');
+    display = getDisplay()
     const equation = display.textContent;
-    firstNumber = equation.charAt(0);
-    operator = equation.charAt(1);
-    secondNumber = equation.charAt(2);
+    const splitEquation = equation.split(operators);
+    console.log(splitEquation);
+    firstNumber = splitEquation[0];
+    operator = splitEquation[1];
+    secondNumber = splitEquation[2];
+    if (negativeNumber){
+        firstNumber = `-${splitEquation[2]}`;
+        operator = splitEquation[3];
+        secondNumber = splitEquation[4];
+    }
     const total = operate(parseInt(firstNumber), parseInt(secondNumber), operator);
-    display.textContent = total;
+    let roundedTotal = total.toFixed(2)
+    display.textContent = roundedTotal;
 });
 let firstNumber = 0;
 let secondNumber = 0;
 let operator = '';
+const operators = /([+\-*/])/g;
+let negativeNumber = false;
